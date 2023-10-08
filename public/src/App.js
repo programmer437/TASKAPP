c1e18b78608ed34401a8b2b452c4192061871251
Gurpreet  Singh <gopi89410gmail.com>
2023-10-08 01:41:36 -0400

    Created add task functionality

--- public/src/App.js
+++ public/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login'; 
 import Tasks from './components/Tasks';
+import EditTask from './components/EditTask'
 
 import './App.css';
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/tasks" element={<Tasks />} />
+          <Route path='/tasks/:id' element={<EditTask/>}/>
         <Route/>
       </Routes>
     </BrowserRouter>
--- public/src/components/Tasks.js
+++ public/src/components/Tasks.js
@@ -1,75 +1,105 @@
-import React,{useEffect,useState}from 'react'
-import { AiTwotoneEdit,AiFillDelete } from "react-icons/ai";
+import React, { useEffect, useState } from 'react';
+import { AiTwotoneEdit, AiFillDelete } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import "../css/Tasks.css";
-
+import '../css/Tasks.css';
 
 export default function Dashboard() {
-  const [input,setInput]=useState('');
-  const [data,setData]=useState(null);
-  useEffect(()=>{
-    
-    async function fetchData(){
-    try {
-      const response=await axios.get("http://localhost:3000/api/v1/tasks",{withCredentials: true});
-      
-      if(response.status===201){
-         setData(response.data);
-        }
-    }catch (error) {
+  const history = useNavigate();
+  const [taskCreated, setTaskCreated] = useState(false);
+  const [input, setInput] = useState('');
+  const [data, setData] = useState(null);
 
-      console.error(error);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('http://localhost:3000/api/v1/tasks', {
+          withCredentials: true,
+        });
+
+        if (response.status === 201) {
+          setData(response.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, [taskCreated]);
+
+  if (!data) return null;
+
+  function editTaskHandle(id) {
+    history(`/tasks/${id}`);
+  }
+
+  async function handleCreateTask(e) {
+    const formData = {
+      name: input,
+      completed: false,
+    }
+
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/api/v1/tasks', formData, {
+        withCredentials: true,
+      });
+      if (response.status === 201) {
+        setTaskCreated(true);
+
+        setInput('');
+        setTimeout(() => {
+          setTaskCreated(false);
+
+        }, 3000); // Change to 10000 milliseconds (10 seconds)
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
-    fetchData();
 
-  },[]);
-  if(!data) return null;
-
-  const taskItems = data.tasks.map((task)=> {
-   
-    return <div className="taskItem" key={task._id}>
-      <p>{task.name}</p> 
-      <div className="icons">
-        <AiTwotoneEdit />
-        <AiFillDelete />
-      </div>
-      
-      </div>
+  const handleChange = ((e) => {
+    setInput(e.target.value);
   })
-  const handleChange=((e)=>{
-    
-    setInput(e.target.value)
-  })
-  console.log(input)
 
-  return(
-    <section className='vh-100'>
-      <div className='container h-100'>
-        <div className='row d-flex bg-light align-items-center justify-content-center h-100'>
-          <div className='createTask d-flex justify-content-center align-items-center col-xl-6 col-md-10 col-sm-11 col-10 '>
-            <input 
-            type='text'
-            name=""
-            value={input}
-            onChange={handleChange}
-            />
-            <button className='btn btn-dark text-center rounded-0'>Create Task</button>
-            
-          </div>
-          <h2>Your Tasks</h2>
-          
-          <div className='taskContainer h-50 d-flex flex-column align-items-center '>
-          
-            {taskItems}
-            
-          </div>
-            
+  const taskItems = data.tasks.map((task) => {
+    return (
+      <div className="taskItem" key={task._id}>
+        <p>{task.name}</p>
+        <div className="icons">
+          <AiTwotoneEdit onClick={() => editTaskHandle(task._id)} />
+          <AiFillDelete />
         </div>
       </div>
+    );
+  })
+  console.log(taskCreated);
 
+
+  return (
+    <section className='vh-100'>
+      <div className='container h-100'>
+        <div className='row d-flex align-items-center justify-content-center h-100'>
+          <div className='createTask d-flex flex-column justify-content-center align-items-center col-xl-6 col-md-10 col-sm-11 col-10 '>
+            <p className='taskTitle'>Task Manager</p>
+            <div className='inputField'>
+              <input
+                type='text'
+                name=""
+                value={input}
+                onChange={handleChange}
+                placeholder='e.g. wash dishes'
+              />
+              <button onClick={handleCreateTask} className='btn  text-center rounded-0'>Create Task</button>
+            </div>
+            {taskCreated && <p className='createdAlert'>Task Created!</p>}
+          </div>
+
+          <div className='taskContainer h-50 d-flex flex-column align-items-center '>
+            {taskItems}
+          </div>
+        </div>
+      </div>
     </section>
-   
-
   )
 }
